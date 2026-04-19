eae594b36a5b60a8497661fc43949f8bf4fe5abb
Miguel <miguel@mrpservices.tech>
2026-04-19 17:21:12 +0200

    first test cicd

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,6 +3,11 @@
 variable "tags" {
   description = "Default tags for all the resources"
   type        = map(string)
+  default = {
+    Project     = "ticketing-service-serverless"
+    Environment = "test"
+    ManagedBy   = "terraform"
+  }
 }
 
 ## ----------------------- lambda variables ----------------------- ##
@@ -10,21 +15,25 @@ variable "tags" {
 variable "lambda_zip_path" {
   description = "Path to the Lambda function zip file"
   type        = string
+  default     = "lambda_function.zip"
 }
 
 variable "lambda_function_name" {
   description = "The name of the Lambda function"
   type        = string
+  default     = "ticketing-service-lambda"
 }
 
 variable "lambda_handler" {
   description = "The entry point for the Lambda function"
   type        = string
+  default     = "lambda_function.handler"
 }
 
 variable "admin_group" {
   description = "cognito admin group name"
   type        = string
+  default     = "Admins"
 }
 
 ## ----------------------- sns ----------------------- ##
